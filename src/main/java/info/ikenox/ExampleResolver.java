e794dddd21d75d09e9a3e68b4602f5da48d7ca62
naoto.ikeno <naoto.ikeno@dena.jp>
2022-09-25 15:32:13 +0900

    implement event example

--- src/main/java/info/ikenox/ExampleResolver.java
+++ src/main/java/info/ikenox/ExampleResolver.java
@@ -1,7 +1,10 @@
 package info.ikenox;
 
+import io.smallrye.graphql.cdi.event.ErrorDataFetch;
+import io.smallrye.graphql.cdi.event.ErrorInfo;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -15,6 +18,11 @@ public class ExampleResolver {
 
   @Query("example")
   public List<Foo> example() {
+    // onError method will receive this error if uncommented-out
+//    if (true) {
+//      throw new RuntimeException("fooError");
+//    }
+
     return List.of(
         new Foo(1),
         new Foo(2),
@@ -23,14 +31,20 @@ public class ExampleResolver {
   }
 
   public List<Bar> bar(@Source List<Foo> fooList) {
-    return fooList.stream().map(foo -> new Bar(foo.value)).toList();
+    // onError method won't receive this error
+    throw new RuntimeException("barError");
   }
 
-  public record Foo(int value) {
+  // exception observer
+  public void onError(@Observes @ErrorDataFetch ErrorInfo errorInfo) {
+    System.out.println("[error] " + errorInfo.getT().getMessage());
+  }
+
+  public record Foo(int fooValue) {
 
   }
 
-  public record Bar(int value) {
+  public record Bar(int barValue) {
 
   }
 }
